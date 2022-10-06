0f43707027e2326ccb2e000cf7f18c35728ce20b
plettboy <ryan.plett@yahoo.com>
2022-10-06 16:23:21 -0700

    form added

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './styles.scss'
-import Header from './Header';
 
 
 
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -156,4 +157,19 @@ storiesOf("Button", module)
       ))
       .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action('onConfirm')} onCancel={action('onCancel')}/>)
       .add("Status", () => <Status message="Deleting" />)
-      .add("Error", () => <Error message="Could not delete appointment." onClose={action('onClose')} />);
+      .add("Error", () => <Error message="Could not delete appointment." onClose={action('onClose')} />)
+      .add("Edit", () => (
+        <Form
+          student="Joey"
+          interviewer={1}
+          interviewers={interviewers}
+          onSave={action('onSave')}
+          onCancel={action('onCancel')}
+        />
+      ))
+      .add("Create", () => (
+        <Form
+        interviewers={interviewers}
+        onSave={"onSave"}
+        onCancel={'onCancel'} />
+      ))
