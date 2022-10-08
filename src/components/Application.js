5a78066359e0eab74ddaeceb942c00756d53b2b1
plettboy <ryan.plett@yahoo.com>
2022-10-07 22:00:21 -0700

    first get request with axios completed

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import "components/Appointment";
 import Appointment from "components/Appointment";
+import axios from 'axios'
 
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+
+const [days, setDays] = useState([]);
 
 const [day, setDay] = useState("Monday")
+
+useEffect(() => {
+  axios.get('/api/days')
+  .then((response) => {
+    setDays(res.data)
+  })
+  .catch((error) => {
+    console.log(error.response.status)
+    console.log(error.response.headers)
+    console.log(error.response.data)
+  })
+})
 
 const appointments = {
   "1": {
